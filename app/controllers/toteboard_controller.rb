4c4b879fa179ad54f8a1ba1e3fd1e5431019a9e4
Susan Wright <wright1@ualberta.ca>
2018-12-14 16:13:46 -0700

    Display list of race days

--- app/controllers/toteboard_controller.rb
+++ app/controllers/toteboard_controller.rb
@@ -1,5 +1,5 @@
 class ToteboardController < ApplicationController
   def index
-    @race_days = HTTParty.get("http://interview.dev.cfdv.net/tracks/CD/race-days.json")
+    @races = HTTParty.get("http://interview.dev.cfdv.net/tracks/CD/race-days.json")
   end
 end
