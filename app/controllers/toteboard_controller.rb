9f8776b9e3b03d5a8f3aaa1ac9c7ac8a275c6823
Susan Wright <wright1@ualberta.ca>
2018-12-15 02:42:19 -0700

    Set default race landing

--- app/controllers/toteboard_controller.rb
+++ app/controllers/toteboard_controller.rb
@@ -4,16 +4,23 @@ class ToteboardController < ApplicationController
       get("#{Rails.configuration.x.api_prefix}/race-days.json").
       parsed_response.
       sort_by { |v| v["Date"] }
+    set_race_day(@races.first["Date"], 1)
+    @race_count = @races.first["NumRaces"]
   end
 
   def race_day
-    @race_day = params.fetch(:race_day)
+    set_race_day(params.fetch(:race_day), params.fetch(:race_num, 1))
     @race_count = params.fetch(:race_count)
-    race_num = params.fetch(:race_num, 1)
+
+    respond_to :js
+  end
+
+  private
+
+  def set_race_day(day, race_num)
+    @race_day = day
     @race = HTTParty.
       get("#{Rails.configuration.x.api_prefix}/race-days/#{@race_day}/races/#{race_num}.json").
       parsed_response
-
-    respond_to :js
   end
 end
