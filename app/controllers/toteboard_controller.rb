b5bba9aefc6bc053c0de419f3daebaf9bd348916
Susan Wright <wright1@ualberta.ca>
2018-12-14 17:04:18 -0700

    AJAX call to fetch individual race from dropdown

--- app/controllers/toteboard_controller.rb
+++ app/controllers/toteboard_controller.rb
@@ -1,5 +1,18 @@
 class ToteboardController < ApplicationController
+  API_PREFIX = "http://interview.dev.cfdv.net/tracks/CD".freeze
+
   def index
-    @races = HTTParty.get("http://interview.dev.cfdv.net/tracks/CD/race-days.json")
+    @races = HTTParty.
+      get("#{API_PREFIX}/race-days.json").
+      parsed_response.
+      sort_by { |v| v["Date"] }
+  end
+
+  def race_day
+    @race = HTTParty.
+      get("#{API_PREFIX}/race-days/#{params[:race_day]}/races/#{params[:race_num]}.json").
+      parsed_response
+
+    respond_to :js
   end
 end
